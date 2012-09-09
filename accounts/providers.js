363d8a2497a8b679b4f3a7f34f04b0504f1ee837
Mike Bannister <notimpossiblemike@gmail.com>
2012-09-09 16:39:11 -0400

    init

---
+++ accounts/providers.js
@@ -0,0 +1,4 @@
+var url = 'http://dev.auth-demo-3.meteor.com';
+
+Meteor.accounts.google.config('610850361368.apps.googleusercontent.com', url);
+Meteor.accounts.twitter.config('MWQD4AhnzGI2WlrB3ZBDw', url);
